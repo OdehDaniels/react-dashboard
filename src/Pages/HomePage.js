93289ecca1ee25baa27fd733e6433c3798528622
Odeh Daniels <odehdaniel1@gmail.com>
2021-02-09 01:10:31 +0100

    Frontend pages init

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,70 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Route, Switch, Redirect } from "react-router-dom";
+import { Routes } from "../routes";
 
-function HomePage() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+// pages
+import Signin from "./frontend/Signin";
+import Signup from "./frontend/Signup";
+import ForgotPassword from "./frontend/ForgotPassword";
+import ResetPassword from "./frontend/ResetPassword";
+import Lock from "./frontend/Lock";
+import NotFoundPage from "./frontend/NotFound";
+import ServerError from "./frontend/ServerError";
 
-export default HomePage
+const FrontEndRoute = ({ component: Component, ...rest }) => {
+  return (
+    <Route {...rest} render={props => ( <> <Component {...props} /> </> ) } />
+  );
+};
+
+// const RouteWithSidebar = ({ component: Component, ...rest }) => {
+//   const [loaded, setLoaded] = useState(false);
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => setLoaded(true), 1000);
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   const localStorageIsSettingsVisible = () => {
+//     return localStorage.getItem('settingsVisible') === 'false' ? false : true
+//   }
+
+//   const [showSettings, setShowSettings] = useState(localStorageIsSettingsVisible);
+
+//   const toggleSettings = () => {
+//     setShowSettings(!showSettings);
+//     localStorage.setItem('settingsVisible', !showSettings);
+//   }
+
+//   return (
+//     <Route {...rest} render={props => (
+//       <>
+//         <Preloader show={loaded ? false : true} />
+//         <Sidebar />
+
+//         <main className="content">
+//           <Navbar />
+//           <Component {...props} />
+//           <Footer toggleSettings={toggleSettings} showSettings={showSettings} />
+//         </main>
+//       </>
+//     )}
+//     />
+//   );
+// };
+
+export default () => (
+  <Switch>
+    <FrontEndRoute exact path={Routes.Signin.path} component={Signin} />
+    <FrontEndRoute exact path={Routes.Signup.path} component={Signup} />
+    <FrontEndRoute exact path={Routes.ForgotPassword.path} component={ForgotPassword} />
+    <FrontEndRoute exact path={Routes.ResetPassword.path} component={ResetPassword} />
+    <FrontEndRoute exact path={Routes.Lock.path} component={Lock} />
+    <FrontEndRoute exact path={Routes.NotFound.path} component={NotFoundPage} />
+    <FrontEndRoute exact path={Routes.ServerError.path} component={ServerError} />
+
+    {/* pages */}
+    
+    <Redirect to={Routes.NotFound.path} />
+  </Switch>
+);
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+
+export const Routes = {
+    // pages
+    Signin: { path: "/u/sign-in" },
+    Signup: { path: "/u/sign-up" },
+    ForgotPassword: { path: "/u/forgot-password" },
+    ResetPassword: { path: "/u/reset-password" },
+    Lock: { path: "/u/lock" },
+    NotFound: { path: "/u/404" },
+    ServerError: { path: "/u/500" },
+
+    // docs
+    // components
+    
+};
