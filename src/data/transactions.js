6ce8ade78cfceb4f0f008d31c0a5ce8f82e611ee
Odeh Daniels <odehdaniel1@gmail.com>
2021-05-08 13:12:51 +0100

    Order table init

--- src/data/transactions.js
+++ src/data/transactions.js
@@ -5,7 +5,8 @@ export default [
     {
         "invoiceNumber": 300500,
         "status": "Paid",
-        "subscription": "Platinum Subscription Plan",
+        "subscription": "Twitter",
+        "qty": "2,000",
         "price": "799,00",
         "issueDate": moment().subtract(1, "days").format("DD MMM YYYY"),
         "dueDate": moment().subtract(1, "days").add(1, "month").format("DD MMM YYYY")
@@ -13,7 +14,8 @@ export default [
     {
         "invoiceNumber": 300499,
         "status": "Paid",
-        "subscription": "Platinum Subscription Plan",
+        "subscription": "Youtube",
+        "qty": "1,430",
         "price": "799,00",
         "issueDate": moment().subtract(2, "days").format("DD MMM YYYY"),
         "dueDate": moment().subtract(2, "days").add(1, "month").format("DD MMM YYYY")
@@ -21,7 +23,8 @@ export default [
     {
         "invoiceNumber": 300498,
         "status": "Paid",
-        "subscription": "Platinum Subscription Plan",
+        "subscription": "Instagram",
+        "qty": "100",
         "price": "799,00",
         "issueDate": moment().subtract(2, "days").format("DD MMM YYYY"),
         "dueDate": moment().subtract(2, "days").add(1, "month").format("DD MMM YYYY")
@@ -29,7 +32,8 @@ export default [
     {
         "invoiceNumber": 300497,
         "status": "Paid",
-        "subscription": "Flexible Subscription Plan",
+        "subscription": "Facebook",
+        "qty": "200",
         "price": "233,42",
         "issueDate": moment().subtract(3, "days").format("DD MMM YYYY"),
         "dueDate": moment().subtract(3, "days").add(1, "month").format("DD MMM YYYY")
@@ -37,7 +41,8 @@ export default [
     {
         "invoiceNumber": 300496,
         "status": "Due",
-        "subscription": "Gold Subscription Plan",
+        "subscription": "Twitter",
+        "qty": "2,000",
         "price": "533,42",
         "issueDate": moment().subtract(1, "day").subtract(1, "month").format("DD MMM YYYY"),
         "dueDate": moment().subtract(1, "day").format("DD MMM YYYY")
@@ -45,7 +50,8 @@ export default [
     {
         "invoiceNumber": 300495,
         "status": "Due",
-        "subscription": "Gold Subscription Plan",
+        "subscription": "Twitter",
+        "qty": "100",
         "price": "533,42",
         "issueDate": moment().subtract(3, "days").subtract(1, "month").format("DD MMM YYYY"),
         "dueDate": moment().subtract(3, "days").format("DD MMM YYYY")
@@ -53,7 +59,8 @@ export default [
     {
         "invoiceNumber": 300494,
         "status": "Due",
-        "subscription": "Flexible Subscription Plan",
+        "subscription": "Instagram",
+        "qty": "1,200",
         "price": "233,42",
         "issueDate": moment().subtract(4, "days").subtract(1, "month").format("DD MMM YYYY"),
         "dueDate": moment().subtract(4, "days").format("DD MMM YYYY")
@@ -61,7 +68,8 @@ export default [
     {
         "invoiceNumber": 300493,
         "status": "Canceled",
-        "subscription": "Gold Subscription Plan",
+        "subscription": "Instagram",
+        "qty": "3400",
         "price": "533,42",
         "issueDate": moment().subtract(20, "days").subtract(1, "month").format("DD MMM YYYY"),
         "dueDate": moment().subtract(20, "days").format("DD MMM YYYY")
@@ -69,7 +77,8 @@ export default [
     {
         "invoiceNumber": 300492,
         "status": "Canceled",
-        "subscription": "Platinum Subscription Plan",
+        "subscription": "Youtube",
+        "qty": "1,000",
         "price": "799,00",
         "issueDate": moment().subtract(2, "months").format("DD MMM YYYY"),
         "dueDate": moment().subtract(3, "months").format("DD MMM YYYY")
@@ -77,7 +86,8 @@ export default [
     {
         "invoiceNumber": 300491,
         "status": "Paid",
-        "subscription": "Platinum Subscription Plan",
+        "subscription": "Instagram",
+        "qty": "5,000",
         "price": "799,00",
         "issueDate": moment().subtract(6, "days").format("DD MMM YYYY"),
         "dueDate": moment().subtract(6, "days").add(1, "month").format("DD MMM YYYY")
